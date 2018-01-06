1b0fc71d3be1bdfe32387ad9bf98924040ebd9ef
Monika <monika@ericsson.com>
2018-01-06 22:01:10 +0530

    first commit

---
+++ DevOpsDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+namespace DevOpsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                {
+                    Console.WriteLine("Please provide path");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    DirectoryInfo dir = new DirectoryInfo(args[0]);
+                    DirectoryInfo[] dirInfo = dir.GetDirectories();
+                    Console.WriteLine();
+                    Console.WriteLine("############################");
+                    Console.WriteLine();
+                    Console.WriteLine("Number of directories : " + dirInfo.Length);
+                    Console.WriteLine();
+                    Console.WriteLine("############################"); Console.WriteLine();
+                    foreach (DirectoryInfo di in dirInfo)
+                    {
+                        Console.WriteLine(di.FullName);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    FileInfo[] files = dir.GetFiles();
+                    Console.WriteLine("############################");
+                    Console.WriteLine();
+                    Console.WriteLine("Number of files : " + files.Length);
+                    Console.WriteLine();
+                    Console.WriteLine("############################"); Console.WriteLine();
+                    foreach (FileInfo fi in files)
+                    {
+                        Console.WriteLine(fi.FullName);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Invalid Path");
+                Console.ReadLine();
+            }
+        }
+    }
+}
